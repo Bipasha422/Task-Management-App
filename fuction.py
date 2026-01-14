2bdc294675771ffb58c32e6a2d263bfebfed1c61
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 00:22:05 +0530

    Delete fuction.py

--- fuction.py
+++
@@ -1,8 +0,0 @@
-def avg():
-    a = int(input("enter your number:"))
-    b = int(input("enter your number: "))
-    c = int(input("enetr your number: "))
-    average = (a+b+c)/3
-    print(average)
-avg()
-avg()
