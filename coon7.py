1113d92b59f47a665628065224832e59bd66d045
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 00:16:10 +0530

    Delete coon7.py

--- coon7.py
+++
@@ -1,9 +0,0 @@
-marks1 = int(input("Enter Marks 1"))
-marks2 = int(input("Enter Marks 2"))
-marks3 = int(input("Enter Marks 3"))
-
-totalp =(100*(marks1+marks2+marks3))/300 
-if(totalp>=40 and marks1>=33 and marks2>=33 and marks3>=33):
-    print("You are pass")
-else:
-    print("you failed,try again later")
