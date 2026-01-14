f32646a37519569a103a82a7b0a059e04878b173
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 00:19:23 +0530

    Delete exceptionhand.py

--- exceptionhand.py
+++
@@ -1,9 +0,0 @@
-try:
- a = int(input("hey,enter a number:"))
- print(a)
- b = int(input("hey,enter another number:"))
- print("Result:",a/b)
-except ZeroDivisionError:
- print("cannot divide by zero")
-except ValueError:
- print("please enter valid number")
