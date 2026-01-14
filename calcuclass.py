6d22b8f46ed1f6cf8a8b8c7f2427e1a83ab58e36
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 00:13:24 +0530

    Delete calcuclass.py

--- calcuclass.py
+++
@@ -1,8 +0,0 @@
-class Calculator:
-    def __init__(self,n):
-        self.n=n
-        def square(self):
-            return self.number ** 2
-            num = int(input("enter a number:"))
-            cal=Calculator(n)
-            print("square of the number is:",cal.Calculator)
