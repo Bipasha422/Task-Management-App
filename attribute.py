153b5566c2e8ff3f11312e2ba7ede7111435dcb7
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 00:11:40 +0530

    Delete attribute.py

--- attribute.py
+++
@@ -1,6 +0,0 @@
-class Employee:
-    language ="python"# this is a class attribute
-    salary=1200 
-harry = Employee()
-harry.language = "java"# this is a instance attribute
-print(harry.language,harry.salary)
