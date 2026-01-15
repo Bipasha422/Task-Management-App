ebc7c85f87deb0fd400dc796ec2ae5ec23a2e955
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 05:57:34 +0530

    Delete union.py

--- union.py
+++
@@ -1,8 +0,0 @@
-def process(items):
-    result =[]
-    for elements in items
-
-    if elements in items
-    if isinstance(elements,int):
-        result.append(f"Number:{elements}")
-    else isinstance(elements,str)
