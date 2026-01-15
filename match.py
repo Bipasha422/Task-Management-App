1102f824a65e445c0961ba8d3919ab7f0b1aa1a0
Bipasha422 <bipashapanda4@gmail.com>
2026-01-15 05:53:22 +0530

    Delete match.py

--- match.py
+++
@@ -1,9 +0,0 @@
-def play(status):
-    match status:
-        case 200:
-            return"ok"
-        case 400:
-            return "better"
-        case 600:
-            return "best"
-print(play(600))
